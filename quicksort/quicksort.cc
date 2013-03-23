cc6346d7cef4ae3ce2cba0fe0815c5bfb5c8ba1c
xluren <xluren@126.com>
2013-03-23 18:10:21 +0800

    add quicksort to algorithm

---
+++ quicksort/quicksort.cc
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+void QuickSort(int a[],int left,int right)
+{
+	int i,j,tmp;
+	if(left<right)
+	{
+		i=left-1;
+		j=right+1;
+		tmp=a[(left+right)/2];
+		while(1)
+		{
+			while(a[++i]<tmp);
+			while(a[--j]>tmp);
+			if(i>=j)
+				break;
+			swap(a[i],a[j]);
+		}
+		QuickSort(a,left,i-1);
+		QuickSort(a,j+1,right);
+	}
+}
+int main()
+{
+	int a[]={1,3,5,7,9,2,4,6,8,0};
+	QuickSort(a,0,sizeof(a)/sizeof(int)-1);
+	for(int i=0;i<sizeof(a)/sizeof(int);i++)
+		cout<<a[i]<<"\t";
+	cout<<endl;
+	return 0;
+}
